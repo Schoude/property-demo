b30027474208901fe7c6b2691c59f7408060e007
Marc <marcbaque1311@gmail.com>
2021-08-04 23:15:16 +0200

    [ FIX ] use Deno.env to get env variables

--- db.ts
+++ db.ts
@@ -1,7 +1,7 @@
-import { config, MongoClient } from "./deps.ts";
+import { MongoClient } from "./deps.ts";
 
 const client = new MongoClient();
-await client.connect(config().MONGO_URI);
+await client.connect(Deno.env.get("MONGO_URI") as string);
 const db = client.database("property");
 
 export { db };
--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 // Dotenv
-export { config } from "https://deno.land/x/dotenv@v2.0.0/mod.ts";
+export * from "https://deno.land/x/dotenv@v2.0.0/load.ts";
 
 // Oak
 export {
