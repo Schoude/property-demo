62b8992130f3954ae30c717d7c19878f8b8792fc
Marc <marcbaque1311@gmail.com>
2021-08-05 01:28:36 +0200

    [ REMOVE ] console log

--- db.ts
+++ db.ts
@@ -1,7 +1,6 @@
 import { MongoClient } from "./deps.ts";
 
 const client = new MongoClient();
-console.log(Deno.env.get("MONGO_URI"));
 await client.connect(Deno.env.get("MONGO_URI") as string);
 const db = client.database("property");
 
