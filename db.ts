074da4fff0ef5f9f4959703a9c56d0dae3be1dbf
Marc <marcbaque1311@gmail.com>
2021-08-05 01:13:10 +0200

    [ REMOVE ] double import

--- db.ts
+++ db.ts
@@ -1,7 +1,7 @@
-import "https://deno.land/x/dotenv/load.ts";
 import { MongoClient } from "./deps.ts";
 
 const client = new MongoClient();
+console.log(Deno.env.get("MONGO_URI"));
 await client.connect(Deno.env.get("MONGO_URI") as string);
 const db = client.database("property");
 
